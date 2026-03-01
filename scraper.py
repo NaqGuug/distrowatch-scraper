03b5e9ea8b14fd83971bbe50208cb2144de1284e
Naq <naq.guug@gmail.com>
2026-03-01 18:02:37 +0200

    feat: Fetch and save images

--- scraper.py
+++ scraper.py
@@ -3,14 +3,16 @@ import requests
 import re
 from datetime import datetime, timezone
 import json
+import os
 
 # Constants
+PARSER: str = "html5lib"
+EXTRACT_DIRECTORY: str = os.getcwd() + "/data"
 DISTOWATCH_URL: str = "https://distrowatch.com/"
 URL_EXTENSION: str = "table.php?distribution="
 REQUEST_HEADERS: dict[str, str] = {
     "User-Agent": "Mozilla Gecko"
 }
-PARSER: str = "html5lib"
 first_number: re.Pattern = re.compile(r"\d+")
 
 
@@ -37,7 +39,7 @@ def get_distros() -> list[str]:
     return distros
 
 
-def extract_distro_data(name: str) -> None:
+def extract_distro_data(name: str, images: bool = True) -> None:
     """Extract distro data and save to files"""
     # Get distro page
     distro_page = requests.get(
@@ -48,7 +50,7 @@ def extract_distro_data(name: str) -> None:
     info_page: BeautifulSoup = distro_soup.find(attrs={"class": "TablesTitle"})
 
     # Distro info saved to json
-    distro_info: dict[str, str | int | float] = {}
+    distro_info: dict = {}
     distro_info["slug"] = name
 
     # Distro full name
@@ -77,33 +79,76 @@ def extract_distro_data(name: str) -> None:
         distro_info[key] = value
 
     # Manual edits to status and popularity
-    distro_info["status"] = distro_info["status"].partition(",")[0]
-    if not distro_info["popularity"]:
-        # Popularity not ranked
-        distro_info["popularity"] = 0
-        distro_info["hitsPerDay"] = 0
-    else:
+    distro_info["status"] = distro_info["status"].partition(" ")[0]
+    try:
         # Get popularity and hits per day
         popularity_partition: tuple[str, str, str] = distro_info["popularity"].partition(" ")
         distro_info["popularity"] = int(popularity_partition[0])
         distro_info["hitsPerDay"] = int(first_number.search(popularity_partition[2]).group())
+    except ValueError:
+        # Popularity not ranked
+        distro_info["popularity"] = 0
+        distro_info["hitsPerDay"] = 0
 
     # Rating
     bold_text = info_page.find_all("b")
-    distro_info["rating"] = 0
-    distro_info["reviewCount"] = 0.0
     try:
+        # Get rating and review count
         distro_info["rating"] = int(bold_text[-1].text)
         distro_info["reviewCount"] = float(bold_text[-2].text)
     except ValueError or KeyError:
-        pass
+        # Not rated
+        distro_info["rating"] = 0
+        distro_info["reviewCount"] = 0.0
 
-    # Distro logo
+    # Image urls
     distro_info["logo"] = DISTOWATCH_URL + info_page.find("img", attrs={"class": "logo"}).get("src")
-    logo: bytes = requests.get(
-        url=distro_info["logo"],
-        headers=REQUEST_HEADERS
-    ).content
+    screenshot_a = info_page.a
+    thumbnail_img = screenshot_a.img
+    if thumbnail_img:
+        distro_info["screenshot"] = DISTOWATCH_URL + screenshot_a.get("href")
+        distro_info["thumbnail"] = DISTOWATCH_URL + thumbnail_img.get("src")
+    else:
+        distro_info["screenshot"] = None
+        distro_info["thumbnail"] = None
+
+    # Image local paths
+    distro_info["localPaths"] = {
+        "logo": f"{EXTRACT_DIRECTORY}/logos/{name}_logo.png" if distro_info["logo"] else None,
+        "thumbnail": f"{EXTRACT_DIRECTORY}/thumbnails/{name}_thumbnail.png" if distro_info["thumbnail"] else None,
+        "screenshot": f"{EXTRACT_DIRECTORY}/screenshots/{name}_screenshot.png" if distro_info["screenshot"] else None
+    }
+
+    # Extract images
+    if images:
+        # Fetch logo
+        logo: bytes = requests.get(
+            url=distro_info["logo"],
+            headers=REQUEST_HEADERS
+        ).content
+
+        with open(distro_info["localPaths"]["logo"], "wb") as lf:
+            lf.write(logo)
+
+        # Fetch thumbnail
+        if distro_info["thumbnail"]:
+            thumbnail: bytes = requests.get(
+                url=distro_info["thumbnail"],
+                headers=REQUEST_HEADERS
+            ).content
+
+            with open(distro_info["localPaths"]["thumbnail"], "wb") as tf:
+                tf.write(thumbnail)
+
+        # Fetch screenshot
+        if distro_info["screenshot"]:
+            screenshot: bytes = requests.get(
+                url=distro_info["screenshot"],
+                headers=REQUEST_HEADERS
+            ).content
+
+            with open(distro_info["localPaths"]["screenshot"], "wb") as sf:
+                sf.write(screenshot)
 
     # Extracted (current time)
     distro_info["extracted"] = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -112,11 +157,12 @@ def extract_distro_data(name: str) -> None:
     with open(f"{name}.json", "w") as json_file:
         json.dump(distro_info, json_file)
 
-    # Save to json, image as png
-    # Ubuntu: 2026-02-21 21:20
-
 
 def main() -> None:
+    os.makedirs(EXTRACT_DIRECTORY, exist_ok=True)
+    os.makedirs(EXTRACT_DIRECTORY + "/logos", exist_ok=True)
+    os.makedirs(EXTRACT_DIRECTORY + "/thumbnails", exist_ok=True)
+    os.makedirs(EXTRACT_DIRECTORY + "/screenshots", exist_ok=True)
     # distros = get_distros()
     # print(len(distros))
     extract_distro_data("gnuinos")
