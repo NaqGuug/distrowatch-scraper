3a6c864464e8406e1de8e06d5e10f43af36ff633
Naq <naq.guug@gmail.com>
2026-03-09 19:26:46 +0200

    feat: Progress bar

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import asyncio
 import aiohttp
 import scraper
 import json
-import time
+from tqdm.asyncio import tqdm_asyncio
 
 
 async def main() -> None:
-    start = time.perf_counter()
     os.makedirs(scraper.EXTRACT_DIRECTORY, exist_ok=True)
     os.makedirs(scraper.EXTRACT_DIRECTORY + "/logos", exist_ok=True)
     os.makedirs(scraper.EXTRACT_DIRECTORY + "/thumbnails", exist_ok=True)
@@ -15,15 +14,12 @@ async def main() -> None:
 
     async with aiohttp.ClientSession() as session:
         distros_names: list[str] = await scraper.get_distros(session)
-        distros_names = distros_names[:100]
+        distros_names = distros_names[:50]
         tasks = [scraper.extract_distro_data(session, name) for name in distros_names]
-        results = await asyncio.gather(*tasks)
+        results = await tqdm_asyncio.gather(*tasks)
 
         with open(f"{scraper.EXTRACT_DIRECTORY}/distros.json", "w") as json_file:
             json.dump(results, json_file, indent=2)
-
-    end = time.perf_counter()
-    print(end - start, "seconds")
 
 
 if __name__ == "__main__":
